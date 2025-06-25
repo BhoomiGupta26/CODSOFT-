dd75061cf9ade8a0197296641873dfeb51dd0ed4
BhoomiGupta26 <bhoomigupta12c@gmail.com>
2025-06-26 00:12:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+/ Highlight nav on scroll
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".nav-link");
+  const sections = document.querySelectorAll("section");
+
+  window.onscroll = () => {
+    let current = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop - 80;
+      if (pageYOffset >= sectionTop) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href").includes(current)) {
+        link.classList.add("active");
+      }
+    });
+  };
+});
